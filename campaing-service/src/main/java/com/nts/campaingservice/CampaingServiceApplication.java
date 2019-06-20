d0dcb17e3bd0486e4cfb7539cf1e228e646487da
Gabriel <gabriel.kirsten@hotmail.com>
2019-06-20 15:41:43 -0400

    Initial commit

---
+++ campaing-service/src/main/java/com/nts/campaingservice/CampaingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nts.campaingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaingServiceApplication.class, args);
+	}
+
+}
