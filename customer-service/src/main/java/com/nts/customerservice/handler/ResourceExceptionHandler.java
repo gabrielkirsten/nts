81eef3708e52bdb920e6c304b8d944610c98c4ee
Gabriel <gabriel.kirsten@hotmail.com>
2019-06-20 21:51:47 -0400

    Added email message

--- customer-service/src/main/java/com/nts/customerservice/handler/ResourceExceptionHandler.java
+++ customer-service/src/main/java/com/nts/customerservice/handler/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.nts.customerservice.handler;
 
+import com.nts.customerservice.exception.EmailAlreadyExistsException;
 import com.nts.customerservice.exception.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,6 +17,11 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity handleResourceNotFound() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity handleEmailAlreadyExistsException(){
+        return ResponseEntity.badRequest().body("Email already exists!");
     }
 
 }
